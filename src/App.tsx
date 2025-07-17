397b0b26bf567f10075c233771057075b5279e66
SaugatKadariya <pamsam297@gmail.com>
2025-07-17 14:46:43 +0545

    env files added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Elements } from "@stripe/react-stripe-js";
 import CdrReport from "./components/CdrReport";
 import { loadStripe } from "@stripe/stripe-js";
 
-
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 function App() {
-  
-const stripePromise = loadStripe('pk_test_51L52aeDo17ticfh1Y93ZuFF7dMAtZgNk7ZcVuKt1Qgf6BLeJutRhCYaaYrn97L2Ji7zwlEWhMhWyjHKhTrPeEe6U00X2dv48jH');
   return (
-     <Elements stripe={stripePromise}>
+    <Elements stripe={stripePromise}>
       <CdrReport />
     </Elements>
   );
